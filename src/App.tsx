f92ecdfabcb1f7980189294470cd1d7485865b6d
Roy <planedriver27@gmail.com>
2020-01-08 23:27:01 -0800

    another component done

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Sup
         </a>
       </header>
     </div>
