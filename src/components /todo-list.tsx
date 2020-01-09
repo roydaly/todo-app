e459f3eadf9761985dd1cab8926fdb46a42dc448
Roy <planedriver27@gmail.com>
2020-01-08 22:49:18 -0800

    next component done

---
+++ src/components /todo-list.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+
+import TodoItem from './todo-item';
+
+import { TodoListInterface } from './../interfaces';
+
+const TodoList = (props: TodoListInterface) => {
+ return (
+  <div className = 'todo-list'>
+   <ul>
+    {props.todos.map((todo) => (
+     <li key={todo.id}>
+      <TodoItem
+       todo={todo}
+       handleTodoUpdate={props.handleTodoUpdate}
+       handleTodoComplete={props.handleTodoComplete}
+       handleTodoBlur={props.handleTodoBlur}
+       handleTodoRemove={props.handleTodoRemove}
+       />
+       </li>
+    ))}
+   </ul>
+  </div> 
+    )
+}
+
+export default TodoList;
+
